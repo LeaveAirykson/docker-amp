ca6451f936b4bc2987277ce4ce050a6578449833
LeaveAirykson <georg.boete@gmail.com>
2024-06-05 06:44:12 +0200

    V1 with ubuntu image

---
+++ README.md
@@ -0,0 +1,7 @@
+# DAMP
+
+A docker setup for running an Apache server with phpMyAdmin and MySQL.
+
+Uses:
+
+- Docker image ubuntu:20.04
---
+++ apache/startup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# set file permission for log files
+chmod -R a+xrw /var/log/apache2
+
+# start fpm modules
+service php8.3-fpm start
+
+# start apache
+apachectl -D FOREGROUND
